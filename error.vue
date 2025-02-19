d76bc4d931f3c6c3087df1af8be23a9ac9bcbe22
knicKnack-Mark <markneilbertiz21@gmail.com>
2025-02-19 20:11:04 +0800

    update error page layout; increase image size, add back navigation prompt, and change home link to todos

--- error.vue
+++ error.vue
@@ -4,11 +4,12 @@
       src="https://cdn.lordicon.com/lltgvngb.json"
       trigger="loop"
       colors="primary:#121331,secondary:#e83a30"
-      style="width:200px;height:200px"
+      style="width:400px;height:300px"
     />
     <h1 class="display-4 fw-bold text-danger">{{ error.statusCode }}</h1>
+    <p class="lead">Go back!</p>
     <p class="lead">{{ error.message || 'Oops! Something went wrong.' }}</p>
-    <NuxtLink to="/" class="btn btn-primary mt-3">Go Home</NuxtLink>
+    <NuxtLink to="/todos" class="btn btn-primary mt-3">Go Home</NuxtLink>
   </div>
 </template>
 
