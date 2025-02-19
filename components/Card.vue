6ed7a5ce1607a98b738ab897b7bb6af6295002ca
knicKnack-Mark <markneilbertiz21@gmail.com>
2025-02-19 23:25:12 +0800

    add custom scrollbar styles and create Card component for better UI presentation

---
+++ components/Card.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="col-12 col-md-6 col-lg-4">
+    <div class="card border-dark">
+      <div class="card-body text-center p-4 p-xxl-5">
+        <!-- Dynamic Icon -->
+        <Icon :name="cardIcon" size="3em" class="text-primary mb-4" />
+        <h4 class="mb-4">{{ cardTitle }}</h4>
+        <p class="mb-4 text-secondary">{{ cardText }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  cardTitle: {
+    type: String,
+    required: true
+  },
+  cardText: {
+    type: String,
+    required: true
+  },
+  cardIcon: {
+    type: String,
+  }
+});
+</script>
